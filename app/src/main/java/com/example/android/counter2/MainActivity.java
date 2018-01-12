1271880cd3a549059cf299e0bd9be57515cffab2
sskreber <zsu.karap@gmail.com>
2018-01-12 16:19:49 +0100

    Replaced  displayForAlcohol (0); with  displayForAlcohol (scoreAlcohol);

--- app/src/main/java/com/example/android/counter2/MainActivity.java
+++ app/src/main/java/com/example/android/counter2/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForAlcohol(0);
+        displayForAlcohol (scoreAlcohol);
     }
 
 
